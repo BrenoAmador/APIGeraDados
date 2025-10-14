af6dc5bc9dba5515d9317c6d42fb2a1d1561518b
Breno Amador <brenoxstyle@gmail.com>
2025-10-14 12:20:21 -0300

    Organizando projeto: limpa .vs, adiciona .gitignore e README

--- obj/Release/net8.0/APITesteDev.AssemblyInfo.cs
+++ obj/Release/net8.0/APITesteDev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APITesteDev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49b5e91e7f28f54d6e88097030a312b90aaf042f")]
 [assembly: System.Reflection.AssemblyProductAttribute("APITesteDev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APITesteDev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ readme.md
@@ -0,0 +1,18 @@
+# APITesteDev
+
+API de teste em ASP.NET Core 8 com JWT e envio de e-mails via SMTP (MailKit).
+
+## Funcionalidades
+
+- Autenticação com JWT
+- Controle de usuários e autorização de rotas
+- Envio de e-mails com anexos
+- Swagger para documentação da API
+- Configuração via `appsettings.json` ou variáveis de ambiente
+
+## Configuração
+
+1. Clone o repositório:
+
+```bash
+git clone https://github.com/BrenoAmador/APITesteDev.git
